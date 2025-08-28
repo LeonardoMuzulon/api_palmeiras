2022b14ef886e8847706a5b49b94b8b1e9fdc8e0
LeonardoMuzulon <104323536+LeonardoMuzulon@users.noreply.github.com>
2025-08-28 17:16:32 -0300

    Update index.js
    nova alteração

--- index.js
+++ index.js
@@ -12,11 +12,22 @@ app.get("/ping", (req, res) => {
   res.status(200).send("pong");
 });
 
-app.get("/gerar-token/:numero", (req, res) => {
+app.get("/gerar-token/:numero/:tipo", (req, res) => {
   const numero = req.params.numero.replace(/\D/g, "");
+  const tipo = req.params.tipo;
 
+  let query;
   const authDateTime = moment.utc().format("YYYY-MM-DD HH:mm:ss") + " ";
-  const query = `authClientID=${authClientId}&identifier=${numero}&authDateTime=${authDateTime}`;
+
+  if (tipo === "memberships") {
+    // Para GetUserMemberships
+    query = `authClientId=${authClientId}&userId=${numero}&authDateTime=${authDateTime}`;
+  } else if (tipo === "ex") {
+    // Para GetUserExByIdentifier
+    query = `authClientID=${authClientId}&identifier=${numero}&authDateTime=${authDateTime}`;
+  } else {
+    return res.status(400).json({ error: "Tipo inválido. Use 'memberships' ou 'ex'." });
+  }
 
   const hash = cryptoJS.SHA256(cryptoJS.enc.Utf8.parse(query + secretAPIKey));
   const authHash = cryptoJS.enc.Base64.stringify(hash);
